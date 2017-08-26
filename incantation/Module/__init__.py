f827bc48d5450f2c06941f1f0df10c2214d6848c
thautwarm <twshere@outlook.com>
2017-08-26 17:16:09 +0800

    CSS-module-complete

--- incantation/Module/__init__.py
+++ incantation/Module/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .. import foreach, groupby, andThen, compose, flow_map, flatten, fastmap
-from ..abst import abstract_object,indent_setter, default_attr
+from .abst import abstract_object,indent_setter, default_attr
 class blockquote(indent_setter, abstract_object):
     """
     See http://materializecss.com/typography.html
--- test.py
+++ test.py
@@ -5,22 +5,25 @@ from incantation.Module.CSS.Color import Indigo
 from incantation.Module.CSS.Helpers import align, left_align, right_align
 from incantation.Module.CSS.Media import video_container
 from incantation.Module import abst
+from incantation.Module import blockquote
 from incantation.Module.CSS.Table import table
 from incantation.Module.abst import default_conf, gen_helper, Seq
 from incantation.template import Page
 a = container()
+try_columns = blockquote("try columns")
+try_table   = blockquote("try tables") 
 t = table( [
             ['thautwarm','twshere', '3.7'],
             ['nightynight','thaut','4.0']
            ],
               ['name','email','GPA'])
 cols    = Seq(
-                col(video_container(src =""), grid(m=6,s=12,l=12)), 
-                col("2", grid(m=6,s=12,l=12)),
-                col("3", grid(m=6,s=12,l=12))
+                col("this is column1", grid(s=12)),
+                col("and this is column2", grid(s=12))
             )
-Row = row(cols,name = "1")
-a.contains(Seq(Row, t))
+
+Row = row(cols)
+a.contains(Seq(try_columns, Row, try_table, t))
 a.setIndent(1)
 page = Page(a)
 print(page)
